edb8e7973d24db047db072be0b3d2d311b20ce4d
amirhussein soori <amirhusseinsoori75@gmail.com>
2021-02-15 14:36:25 +0330

    AlarmManager

---
+++ AlarmHandler.kt
@@ -0,0 +1,37 @@
+package com.amirhusseinsoori.AlarmHandler
+
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.amirhusseinsoori.broadcastreceiver
+
+class AlarmHandler(var _Context: Context) {
+
+
+    @SuppressLint("ShortAlarm")
+    fun setAlarmManger() {
+
+
+        val intent = Intent(_Context, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(_Context, 0, intent, 0)
+        val am = _Context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        //repeat  task in 3 minutes
+        am.setRepeating(
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            10 * 100.toLong(),
+            1000 * 60 * 5.toLong(),
+            pendingIntent
+        )
+    }
+
+    fun cancelAlarmManger() {
+        val intent = Intent(_Context, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(_Context, 2, intent, 0)
+        val am = _Context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        am.cancel(pendingIntent)
+    }
+
+
+}
---
+++ AlarmReceiver.kt
@@ -0,0 +1,36 @@
+package com.amirhusseinsoori.broadcastreceiver
+
+
+
+
+
+
+class AlarmReceiver() : BroadcastReceiver() {
+   
+
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        
+        Log.e("AlarmReceiver", "onReceive: ")
+
+
+    }
+
+
+
+
+ 
+
+
+}
+
+
+
+
+
+
+
+
+
